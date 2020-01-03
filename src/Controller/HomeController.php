3123dab7dfa0c0a6fd79b3ea448ae9ae6a96daea
fourre <fourre.geo@gmail.com>
2020-01-03 16:28:23 +0100

    modif du homecontroller et du home.twig.html

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,5 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-
+    public function index(){
+        return $this->render('home.html.twig');
+    }
 }
