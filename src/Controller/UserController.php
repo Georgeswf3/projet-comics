63a9bec93e61e0f2551f30076a78d6111cc0186a
steve68130 <steve.lacour21.03@gmail.com>
2020-01-07 13:44:22 +0100

    Routes user + mise à jour sécurité

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,29 @@
 namespace App\Controller;
 
 
-class UserController
-{
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 
+class UserController extends AbstractController
+{
+    private $userRepo;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepo = $userRepository;
+    }
+
+
+// vue page home utilisateur lambda
+    public function homeAction(Request $request, Security $security){
+
+        if ($security->getUser()){
+        $user = $this->userRepo->findOneBy(['email' =>$security->getUser()->getUsername()]);}
+        else{
+            $user = null;
+        }
+        return $this->render('user_home.html.twig', ["user" => $user]);
+    }
 }
