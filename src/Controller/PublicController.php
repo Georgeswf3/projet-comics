5aca30cbbe77c5d4af203cd3306faad771228d9d
steve68130 <steve.lacour21.03@gmail.com>
2020-01-10 14:19:14 +0100

    mise en place de la v2

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class PublicController
+{
+
+
+    public function indexAction()
+    {
+    }
+
+
+
+    public function articlesAction()
+    {
+    }
+
+    public function articleAction($id)
+    {
+    }
+
+    public function fanArtsAction()
+    {
+    }
+
+    public function fanArtAction($id)
+    {
+    }
+
+    public function signupAction()
+    {
+    }
+
+
+
+}
