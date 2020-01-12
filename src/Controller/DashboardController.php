530a2ee06e9feea98465c74ba2fbf485642c7147
fourre <fourre.geo@gmail.com>
2020-01-12 08:41:58 +0100

    refait le controller et le template du create article + template article et articles.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,10 +4,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class DashboardController extends AbstractController
 {
@@ -25,8 +28,33 @@ class DashboardController extends AbstractController
         return $this->render('home.html.twig', ["user" => $user]);
     }
 
-    public function articleCreate()
+    public function articleCreate(Request $request)
     {
+        $slugger = new AsciiSlugger();
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $article = $form->getData();
+            $actualTitle = $article->getTitle();
+            $slug = strtolower($slugger->slug($actualTitle));
+            $article->setSlug($slug);
+
+//            $image = $form['image']->getData();
+//            if($image){
+//                $originalFileName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+//                $newUniqueFileName = $originalFileName."-".uniqid().'.'.$image->guessExtension();
+//                $image->move($this->getParameter('uploaded-images'), $newUniqueFileName);
+//                $article->setImage($newUniqueFileName);
+//            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute("dashboard_home");
+        }
+
+        return $this->render("dashboard/pages/dashboard_create_article.html.twig", ["articleForm" => $form->createView()]);
     }
 
     public function articleUpdate()
