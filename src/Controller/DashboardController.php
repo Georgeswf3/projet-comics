408937cf40785b977e4b9a307f5a5bea7d6a04e7
fourre <fourre.geo@gmail.com>
2020-01-12 20:32:33 +0100

    reconstruction du editor create ainsi que du article create (template et controller) acces bdd ok

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,14 +4,20 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
+use App\Entity\Editor;
+use App\Form\ArticleType;
+use App\Form\EditorType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class DashboardController extends AbstractController
 {
     private $userRepository;
+
 
     public function __construct(UserRepository $userRepository)
     {
@@ -25,8 +31,29 @@ class DashboardController extends AbstractController
         return $this->render('home.html.twig', ["user" => $user]);
     }
 
-    public function articleCreate()
+    public function articleCreate(Request $request, Security $security)
     {
+//        $slugger = new AsciiSlugger();
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $article = $form->getData();
+//            $actualTitle = $article->getTitle();
+//            $slug = strtolower($slugger->slug($actualTitle));
+//            $article->setSlug($slug);
+            //récupérer mon user dans la BDD
+//            $author = $this->userRepository->findOneBy(['email' => $security->getUser()->getUsername()]);
+//            $article->setAuthor($author);
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute("dashboard_home");
+        }
+
+        return $this->render("dashboard/pages/dashboard_create_article.html.twig", ["articleForm" => $form->createView()]);
     }
 
     public function articleUpdate()
@@ -45,8 +72,21 @@ class DashboardController extends AbstractController
     {
     }
 
-    public function editorsCreate()
+    public function editorsCreate(Request $request)
     {
+        $editor = new Editor();
+        $form = $this->createForm(EditorType::class, $editor);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $editor = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($editor);
+            $entityManager->flush();
+            return $this->redirectToRoute("dashboard_home");
+        }
+        return $this->render("dashboard/pages/dashboard_create_editor.html.twig", ["editorForm" => $form->createView()]);
+
     }
 
     public function editorsUpdate()
