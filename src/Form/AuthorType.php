b6b2869480f16ba519c8ed876e3ca1b1c13c9dc3
steve68130 <steve.lacour21.03@gmail.com>
2020-01-14 10:31:14 +0100

    création formulaire authors

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -2,30 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
+use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ArticleType extends AbstractType
+class AuthorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('article_title')
-            ->add('article_text')
-            ->add('article_date')
-            ->add()
-            ->add('isConfirmed')
-            ->add('ajouter', SubmitType::class)
+            ->add('author_name')
+            ->add('author_first_name')
+            ->add('facebook_page')
+            ->add('author_image')
+            ->add('creation_image')
+            ->add('article_id')
+            ->add('jobs')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Article::class,
+            'data_class' => Author::class,
         ]);
     }
 }
