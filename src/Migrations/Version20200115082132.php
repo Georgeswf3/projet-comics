d98f35f4aa745dae9f0ad0f2f6a15926da91c068
geoffrey112 <geoffrey.legall@orange.fr>
2020-01-15 09:28:03 +0100

    design page inscription

--- src/Migrations/Version20200115082132.php
+++ src/Migrations/Version20200115082132.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200114155417 extends AbstractMigration
+final class Version20200115082132 extends AbstractMigration
 {
     public function getDescription() : string
     {
